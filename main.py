cd56f9ea4c7428695af5b0c00ff652ff1d4a530d
Ihor <ihor.renewator@gmail.com>
2026-01-17 15:54:40 +0200

    Інтеграція нових ендпоінтів

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from a2wsgi import WSGIMiddleware
 from starlette.types import ASGIApp
 from typing import cast
-from core.routers import learn_router, auth_router, teacher_router
+from core.routers import learn_router, auth_router, teacher_router, admin_router
 from core.stats.app import dash_app
 
 
@@ -14,6 +14,7 @@ app.mount('/stats', cast(ASGIApp, WSGIMiddleware(dash_app.server)), name='Stats'
 app.include_router(auth_router)
 app.include_router(learn_router)
 app.include_router(teacher_router)
+app.include_router(admin_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
